87f4683d12c6948e6c1f7fc2267686005f392d18
Mohamed <fouademail@gmail.com>
2024-09-16 16:20:43 +0300

    the band project

---
+++ main.js
@@ -0,0 +1,28 @@
+
+// let landingPage = document.querySelector(".landing-page");
+// let imgsArray = [ "background1.jpg", "background2.jpg", "background3.jpg" ];
+
+
+// function randomizeImg () {
+
+//   if ( backgroundOption === true) {
+
+//     backgroundInterval = setInterval( () => {
+//       let randomNum = Math.floor( Math.random() * imgsArray.length );
+//       landingPage.style.backgroundImage = `url(/photos/${imgsArray[randomNum]})`
+//     }, 5000)
+//   }
+
+// }
+// randomizeImg();
+
+
+// change background image //
+
+let headingImg = document.querySelector(".heading");
+let arrayImg = [ "back1.jpg", "back2.jpg", "back3.jpg", "back4.jpg", "back5.jpg", "back6.jpg" ];
+
+setInterval( () => {
+  let randomNum = Math.floor( Math.random() * arrayImg.length );
+  headingImg.style.backgroundImage = `url(../images/${arrayImg[randomNum]})`
+}, 5000)
